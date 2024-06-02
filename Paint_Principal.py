46ba69a45227d995eab5f82b5fa184b38a7fde83
LeTuman26 <martinezvegaalessandro@gmail.com>
2024-06-01 22:36:16 -0600

    Funciones agregadas

--- Paint_Principal.py
+++ Paint_Principal.py
@@ -7,6 +7,7 @@ class PixelArtPaint:  # la clase para la ventana principal (Y unica ventana)
         self.root = root
         self.pixel_size = pixel_size  # Pixel_size es la que define el tamaño de cada celda en la que se pinta
         self.grid_size = grid_size  # Grid_size es la cantidad de celdas a lo largo de cada eje, permite definir cuantas celdas seran
+        self.original_pixel_size = pixel_size
         # Lista donde se tiene definido el color segun el numero
         self.colors = {
             0: "white",
@@ -27,12 +28,22 @@ class PixelArtPaint:  # la clase para la ventana principal (Y unica ventana)
         self.grid_matrix = [[0 for _ in range(self.grid_size)] for _ in range(self.grid_size)]
 
         ancho_ventana = 850  # Ancho de la ventana principal
-        alto_ventana = 600  # Alto de la ventana principal
+        alto_ventana = 700  # Alto de la ventana principal
         self.root.geometry(f"{ancho_ventana}x{alto_ventana}")  # Da el tamaño de la ventana
         self.root.resizable(False, False)  # Impide al usuario modificar las dimensiones de la ventana
 
-        self.canvas = tk.Canvas(root, width=self.pixel_size * self.grid_size, height=self.pixel_size * self.grid_size)
+        self.canvas_frame = tk.Frame(root)
+        self.canvas_frame.pack()
+
+        self.canvas = tk.Canvas(self.canvas_frame, width=455, height=455, scrollregion=(0, 0, self.pixel_size * self.grid_size, self.pixel_size * self.grid_size))
         self.canvas.pack()
+
+        self.h_scrollbar = tk.Scrollbar(self.canvas_frame, orient=tk.HORIZONTAL, command=self.canvas.xview)
+        #self.h_scrollbar.pack(side=tk.BOTTOM, fill=tk.X)
+        self.v_scrollbar = tk.Scrollbar(self.canvas_frame, orient=tk.VERTICAL, command=self.canvas.yview)
+        #self.v_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+        self.canvas.config(xscrollcommand=self.h_scrollbar.set, yscrollcommand=self.v_scrollbar.set)
 
         self.canvas.bind("<Button-1>",
                          self.paint)  # Esta y la siguiente son la configuracion de cuando se da clic con el mouse o si se mantiene presionado
@@ -43,15 +54,18 @@ class PixelArtPaint:  # la clase para la ventana principal (Y unica ventana)
         self.create_print_button()
         self.create_save_button()
         self.create_load_button()
+        self.create_zoom_buttons()
 
     def create_grid(self):  # Encargada de generar la cuadricula o lienzo para dibujar
+        self.canvas.delete("grid")
         for i in range(self.grid_size):
             for j in range(self.grid_size):
                 x0 = i * self.pixel_size
                 y0 = j * self.pixel_size
                 x1 = x0 + self.pixel_size
                 y1 = y0 + self.pixel_size
-                self.canvas.create_rectangle(x0, y0, x1, y1, outline="gray", fill="white")
+                color = self.colors[self.grid_matrix[j][i]]
+                self.canvas.create_rectangle(x0, y0, x1, y1, outline="gray",fill=color, tags="grid")
 
     def create_color_buttons(self):  # crea primero un frame(un tipo de espacio) para los botones
         button_frame = tk.Frame(self.root)
@@ -73,6 +87,16 @@ class PixelArtPaint:  # la clase para la ventana principal (Y unica ventana)
     def create_load_button(self):
         load_button = tk.Button(self.root, text="Load Matrix", command=self.load_matrix)
         load_button.place(x=50,y=100)
+
+    def create_zoom_buttons(self):
+        zoom_frame = tk.Frame(self.root)
+        zoom_frame.pack(pady=10)
+
+        zoom_in_button = tk.Button(zoom_frame, text="Zoom In", command=self.zoom_in)
+        zoom_in_button.grid(row=0, column=0)
+
+        zoom_out_button = tk.Button(zoom_frame, text="Zoom Out", command=self.zoom_out)
+        zoom_out_button.grid(row=0, column=1)
 
     def seleccion_color(self, color_index):  # actualiza la variable que guarda el color
         self.selected_color_index = color_index
@@ -136,6 +160,21 @@ class PixelArtPaint:  # la clase para la ventana principal (Y unica ventana)
                     y += 1
             # Imprime un mensaje en la consola indicando que la matriz se ha cargado desde el archivo
             print(f"Matrix loaded from {file_path}")
+
+    def zoom_in(self):
+        self.pixel_size *= 2  # Duplica el tamaño de los píxeles
+        self.canvas.config(scrollregion=(0, 0, self.pixel_size * self.grid_size, self.pixel_size * self.grid_size))
+        self.create_grid()
+        self.h_scrollbar.pack(side=tk.BOTTOM, fill=tk.X)
+        self.v_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        print(self.pixel_size)
+
+    def zoom_out(self):
+        self.pixel_size = self.original_pixel_size  # Restaura el tamaño original de los píxeles
+        self.canvas.config(scrollregion=(0, 0, self.pixel_size * self.grid_size, self.pixel_size * self.grid_size))
+        self.create_grid()
+        self.h_scrollbar.pack_forget()
+        self.v_scrollbar.pack_forget()
 
 
 if __name__ == "__main__":  # Aqui es donde genera la ventana y mantiene el ciclo de la ventana principal
